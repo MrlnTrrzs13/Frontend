860b417481cef26cd176f281a845d64a5287f88c
Brandon Torres <brand.trrzs13@gmail.com>
2024-03-31 14:53:59 -0600

    Siguiendo el proceso

---
+++ BuonaVitaNutricion/js/calcular-imc.js
@@ -0,0 +1,34 @@
+var pacientes = document.querySelectorAll(".paciente");
+
+for (var i = 0; i < pacientes.length; i++) {
+
+    var paciente = pacientes[i];
+
+    var peso = paciente.querySelector(".info-peso").textContent;
+    var altura = paciente.querySelector(".info-altura").textContent;
+    var tdIMC = paciente.querySelector(".info-imc");
+
+    pesoEsValido = true;
+    alturaEsValida = true;
+
+    if ((peso < 0) || (peso > 1000)) {
+        console.log("Peso Incorrecto.");
+        tdIMC.textContent = "Peso Incorrecto";
+        pesoEsValido = false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+    
+    if ((altura < 0) || (altura > 4.00)) {
+        console.log("Altura Incorrecta.");
+        tdIMC.textContent = "Altura Incorrecta";
+        alturaEsValida = false;
+        paciente.classList.add("paciente-incorrecto");
+    }
+    
+    if (pesoEsValido && alturaEsValida) {
+        var imc = peso / (altura * altura); 
+        tdIMC.textContent = imc.toFixed(2);
+}
+
+
+}
---
+++ BuonaVitaNutricion/js/form.js
@@ -0,0 +1,30 @@
+var botonAdicionar = document.querySelector("#adicionar-paciente");
+botonAdicionar.addEventListener("click",function(event){
+    event.preventDefault();
+
+    var form = document.querySelector("#form-adicional");
+    var nombre =form.nombre.value;
+    var peso =form.peso.value;
+    var altura =form.altura.value;
+    var gordura =form.gordura.value;
+    var tabla = document.querySelector("#tabla-pacientes");
+
+    pacienteTr = document.createElement("tr");
+    nombreTd = document.createElement("td");
+    pesoTd = document.createElement("td");
+    alturaTd = document.createElement("td");
+    gorduraTd = document.createElement("td");
+    imcTd = document.createElement("td");
+
+    nombreTd.textContent = nombre;
+    pesoTd.textContent = peso;
+    alturaTd.textContent = altura;
+    gorduraTd.textContent = gordura;
+
+    pacienteTr.appendChild(nombreTd);
+    pacienteTr.appendChild(pesoTd);
+    pacienteTr.appendChild(alturaTd);
+    pacienteTr.appendChild(gorduraTd);
+
+    tabla.appendChild(pacienteTr);
+});
